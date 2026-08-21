78cc7a68ab8c29d395008a1937982370d9a2164b
MaferVictoria <maria.victoria@rockinmedia.es>
2026-08-21 11:46:45 -0600

    Delete app/meta/page.js

--- app/meta/page.js
+++
@@ -1,5 +0,0 @@
-import DashboardShell from '@/components/DashboardShell';
-
-export default function Page() {
-  return <DashboardShell platform="meta" title="Meta Ads" subtitle="Gasto y conversión reportados por Meta." />;
-}
